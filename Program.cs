546c26e3fbcceb1753f67801a78ba8890e02325a
WilliamBerlin76 <william.berlin76@gmail.com>
2020-08-27 09:16:44 -0500

    loops

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var names = new List<string> { "<name>", "Boobie", "Bobby" };
+
+            names.Add("frank");
+            names.Remove("<name>");
+            
+            foreach (string name in names)
+            {
+                Console.WriteLine($"Hello {name}");
+            }
+        }
+    }
+}
